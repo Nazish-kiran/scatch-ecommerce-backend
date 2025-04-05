f9885fd0b2ca230c8c5dda797904ffb946687f5a
NazishKiran <nazishkirancosmos123@gmail.com>
2025-04-05 18:51:13 +0500

    admin panel

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,9 +1,24 @@
 import express from "express";
+import upload from "../config/multer-config.js";
+import productModel from "../models/product-model.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("product route");
+router.post("/create", upload.single("image"), async (req, res) => {
+  let { name, price, discount, bgcolor, panelcolor, textcolor } = req.body;
+  if (!req.file || !name || !price || !bgcolor || !panelcolor || !textcolor) {
+    req.flash("error", "All fields are required.");
+    return res.redirect("/owners/admin"); // Redirect if data is missing
+  }
+  let product = await productModel.create({
+    image: req.file.buffer,
+    name,
+    price,
+    discount,
+    bgcolor,
+    panelcolor,
+    textcolor,
+  });
 });
 
 export default router;
